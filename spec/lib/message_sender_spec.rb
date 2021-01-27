d3404a8d10c1f81921a58f01dd3b5bbc2ea2e5f0
Martin Mena <mmena@twilio.com>
2021-01-27 15:53:07 -0500

    Fix CI tests

--- spec/lib/message_sender_spec.rb
+++ spec/lib/message_sender_spec.rb
@@ -6,7 +6,11 @@ require_relative '../../lib/message_sender'
 RSpec.describe MessageSender do
   describe '.send_message' do
     it 'sends a message to the customer' do
-      VCR.use_cassette('send_sms_to_customer') do
+      VCR.use_cassette(
+        'send_sms_to_customer',
+        allow_playback_repeats: true,
+        match_requests_on: %i[method uri]
+      ) do
         message = described_class.send_message('1', 'example.com', '+12025550116', 'notification')
         expect(message.status).to eq('queued')
       end
--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -1,6 +1,7 @@
 VCR.configure do |configure|
   configure.cassette_library_dir = "spec/fixtures/cassettes"
   configure.hook_into :webmock
+  configure.ignore_localhost = true
   configure.register_request_matcher :uri do |request1, request2|
     request1.uri.match(request2.uri)
   end
